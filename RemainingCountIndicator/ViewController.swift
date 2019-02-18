030887364465a134499241ab7b1cfce3074800bf
Shima <shima.jin@icloud.com>
2019-02-18 21:00:04 +0900

    Initial Commit

---
+++ RemainingCountIndicator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemainingCountIndicator
+//
+//  Created by jinsei_shima on 2019/02/18.
+//  Copyright © 2019 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
